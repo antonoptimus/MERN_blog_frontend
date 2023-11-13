11ac89c0ead3466dfc114523d8a328e48a85b7cb
Anton Eliseev <anton_saintp@mail.ru>
2023-11-13 14:02:42 +0300

    user info

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Container from "@mui/material/Container";
 import { Header } from "./components/Header";
 import { Home } from "./pages/Home";
+import { UserInfo } from "./components/UserInfo";
 function App() {
   return (
     <>
