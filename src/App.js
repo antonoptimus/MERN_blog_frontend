3b104c41a7623fa87e93a985f8feda6d0c4a7a3f
Anton Eliseev <anton_saintp@mail.ru>
2023-11-13 19:28:29 +0300

    styled logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Container from "@mui/material/Container";
 import { Header } from "./components/Header";
 import { Home } from "./pages/Home";
-import { UserInfo } from "./components/UserInfo";
+
 function App() {
   return (
     <>
-      {/* <Header /> */}
-      <Container maxWidth="lg">
-        <Home />
-      </Container>{" "}
+        <Header />
+        <Container maxWidth="lg">
+          <Home />
+        </Container>
     </>
   );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import Button from "@mui/material/Button";
 import { Container } from "@mui/material";
 import styles from "./Header.module.scss";
-
+import ListAltIcon from "@mui/icons-material/ListAlt";
 export const Header = () => {
   return (
     <div className={styles.root}>
       <Container maxWidth="lg">
         <div className={styles.inner}>
-          <a className={styles.logo} href="/">MERN BLOG</a>
+            <a className={styles.logo} href="/">
+            <ListAltIcon />
+              MERN BLOG
+            </a>
           <div className={styles.buttons}>
             <Button variant="outlined">Войти</Button>
             <Button variant="contained">Создать аккаунт</Button>
