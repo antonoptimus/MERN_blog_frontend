cc37ccc3ffbb70ff9d1acf80a77bed592b94ce4a
Anton Eliseev <anton_saintp@mail.ru>
2023-11-13 13:37:10 +0300

    side comments

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Container from "@mui/material/Container";
 import { Header } from "./components/Header";
+import { Home } from "./pages/Home";
 function App() {
   return (
     <>
-      <Header />
-      <Container maxWidth="lg"></Container>{" "}
+      {/* <Header /> */}
+      <Container maxWidth="lg">
+        <Home />
+      </Container>{" "}
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,3 +2,31 @@ import React from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Grid from "@mui/material/Grid";
+import { SideComments } from "../SideComments";
+
+export const Home = () => {
+  return (
+    <>
+      <Grid xs={4} item>
+        <SideComments
+          items={[
+            {
+              user: {
+                fullName: "Борис Абраков",
+                avararUrl: "https://mui.com/static/images/avatar/6.jpg",
+              },
+              text: "Тестовый комментарий Бориса",
+            },
+            {
+              user: {
+                fullName: "Екатерина Смирнова",
+                avararUrl: "https://mui.com/static/images/avatar/4.jpg",
+              },
+              text: "Тестовый комментарий Екатерины",
+            },
+          ]}
+        />
+      </Grid>
+    </>
+  );
+};
