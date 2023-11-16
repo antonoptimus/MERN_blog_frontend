218166d409fd1c302bfd010a2b434b42d3edd574
Anton Eliseev <anton_saintp@mail.ru>
2023-11-16 11:14:21 +0300

    getting and outputting tags from the backend

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,14 +7,19 @@ import { Post } from "../components/Post";
 import { TagsBlock } from "../components/TagsBlock";
 import { CommentsBlock } from "../components/CommentsBlock";
 import axios from "../axios";
-import { useDispatch } from "react-redux";
-import { fetchPosts } from "../redux/slices/post";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPosts, fetchTags } from "../redux/slices/post";
 
 export const Home = () => {
   const dispatch = useDispatch();
+  const { posts, tags } = useSelector((state) => state.posts);
+
+  const isPostsLoading = posts.status === "loading";
+  const isTagsLoading = posts.tags === "loading";
 
   useEffect(() => {
     dispatch(fetchPosts());
+    dispatch(fetchTags());
   }, []);
 
   return (
@@ -29,30 +34,26 @@ export const Home = () => {
       </Tabs>
       <Grid container spacing={4}>
         <Grid xs={8} item>
-          {[...Array(5)].map(() => (
-            <Post
-              id={1}
-              title="Заголовок статьи"
-              imageUrl="https://pclegko.ru/wp-content/uploads/2020/11/5753173610332160.png"
-              user={{
-                avatarUrl:
-                  "https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png",
-                fullName: "Роман Петров",
-              }}
-              createdAt={"12 июня 2023 г."}
-              viewsCount={15}
-              commentsCount={1}
-              tags={["react", "rtk", "typescript"]}
-              isLoading={true}
-              isEditable
-            />
-          ))}
+          {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) =>
+            isPostsLoading ? (
+              <Post key={index} isLoading={true} />
+            ) : (
+              <Post
+                id={obj._id}
+                title={obj.title}
+                imageUrl="https://pclegko.ru/wp-content/uploads/2020/11/5753173610332160.png"
+                user={obj.user}
+                createdAt={obj.createdAt}
+                viewsCount={obj.viewsCount}
+                commentsCount={obj.commentsCount}
+                tags={obj.tags}
+                isEditable
+              />
+            )
+          )}
         </Grid>
         <Grid xs={4} item>
-          <TagsBlock
-            items={["react", "typescript", "заметки"]}
-            isLoading={false}
-          />
+          <TagsBlock items={tags.items} isLoading={isTagsLoading}/>
           <CommentsBlock
             items={[
               {
--- src/redux/slices/post.js
+++ src/redux/slices/post.js
@@ -6,6 +6,11 @@ export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
   return data;
 });
 
+export const fetchTags = createAsyncThunk("posts/fetchTags", async () => {
+  const { data } = await axios.get("/tags");
+  return data;
+});
+
 const initialState = {
   posts: {
     items: [],
@@ -21,6 +26,34 @@ const postsSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    // Получение постов
+    builder.addCase(fetchPosts.pending, (state) => {
+      state.posts.items = [];
+      state.posts.status = "loading";
+    });
+    builder.addCase(fetchPosts.fulfilled, (state, action) => {
+      state.posts.items = action.payload;
+      state.posts.status = "loaded";
+    });
+    builder.addCase(fetchPosts.rejected, (state) => {
+      state.posts.items = [];
+      state.posts.status = "error";
+    });
+    // Получение тэгов
+    builder.addCase(fetchTags.pending, (state) => {
+      state.tags.items = [];
+      state.tags.status = "loading";
+    });
+    builder.addCase(fetchTags.fulfilled, (state, action) => {
+      state.tags.items = action.payload;
+      state.tags.status = "loaded";
+    });
+    builder.addCase(fetchTags.rejected, (state) => {
+      state.tags.items = [];
+      state.tags.status = "error";
+    });
+  },
 });
 
 export const postsReducer = postsSlice.reducer;
