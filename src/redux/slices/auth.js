03104bafdf2339415b4a270e2d13e5c05f854e13
Anton Eliseev <anton_saintp@mail.ru>
2023-11-16 13:31:03 +0300

    authorization

---
+++ src/redux/slices/auth.js
@@ -0,0 +1,38 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "../../axios";
+
+export const fetchAuth = createAsyncThunk(
+  "auth/fetchAuth",
+  async (params) => {
+    const { data } = await axios.post("/auth/login", params);
+    return data;
+  }
+);
+
+const initialState = {
+  data: null,
+  status: 'loading',
+};
+
+const authSlice = createSlice({
+   name: "auth",
+   initialState,
+   reducers: {},
+   extraReducers: (builder) => {
+     builder.addCase(fetchAuth.pending, (state) => {
+       state.data = null;
+       state.status = "loading";
+     });
+     builder.addCase(fetchAuth.fulfilled, (state, action) => {
+       state.data = action.payload;
+       state.status = "loaded";
+     });
+     builder.addCase(fetchAuth.rejected, (state) => {
+       state.data = null;
+       state.status = "error";
+     });
+   },
+ });
+ 
+ export const authReducer = authSlice.reducer;
+ 
